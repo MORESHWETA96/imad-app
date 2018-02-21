6dca463a5d73afb6b36f48ce56008ea709e1972d
MORESHWETA96 <36556974+MORESHWETA96@users.noreply.github.com>
2018-02-21 11:20:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,11 @@
 console.log('Loaded!');
 //change the value 
 var element = document.getElementById('main-text');
-element.innerHTML = 'NEW VALUE'
+element.innerHTML = 'NEW VALUE';
+
+//change to the madi
+var elementimg = document.getElementById('madi');
+elementimg.onClick = function()
+{
+    elementimg.style.marginleft='100px';
+};
