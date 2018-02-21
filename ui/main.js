59819f0b6d489153784c2de522c46cd1e602fe4f
MORESHWETA96 <36556974+MORESHWETA96@users.noreply.github.com>
2018-02-21 11:24:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,5 +6,5 @@ element.innerHTML = 'NEW VALUE';
 //change to the madi
 var img = document.getElementById('madi');
 img.onClick = function(){
-    img.style.marginleft='100px';
+    img.style.marginLeft='100px';
 };
