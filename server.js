9eb5b579020d948c0e5e2533f183fc8a39a12f49
MORESHWETA96 <36556974+MORESHWETA96@users.noreply.github.com>
2018-02-22 07:59:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ Infodesk@bigdealfinance.com
 
     function createTemplate (data){
         var title = data.title;
-        var date = data.date;
+      
         var heading = data.heading;
         var content = data.content;
       var htmlTemplate =`
@@ -79,7 +79,7 @@ Infodesk@bigdealfinance.com
                 </div>
                 <hr />
                 <div>
-                    ${date}
+                    
                 </div>
                 <div>
                     ${content}
